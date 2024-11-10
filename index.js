59258333ca0fdd2d372a96843d0eeef30def12f8
Jinhwang SHIN <gimmali751@gmail.com>
2024-11-10 19:48:23 +0900

    Update

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function run() {
-    document.querySelector('button').remove()
+    document.querySelector('article').innerHTML='<button-wrap />'
+    document.querySelector('footer').innerHTML=''
     var btn_left = document.createElement('button')
     var btn_right = document.createElement('button')
     var span_progress = document.createElement('p')
-    var comment_progress = document.createElement('p')
+    // var comment_progress = document.createElement('p')
     span_progress.innerText = '0.0%'
     span_progress.style.color = '#fff'
     span_progress.style.width = '90%'
     span_progress.style.background = `#000`
-    comment_progress.style.width = '100%'
+    // comment_progress.style.width = '100%'
     const obj={
         a1:[[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]],
         a2:[[,,],[,,],[,,],[,,],[,,],[,,],[,,],[,,],[],[],[],[],[],[],[],[]],
@@ -20,7 +21,7 @@ function run() {
     const queue = []
     var popped = []
     var progress = 0
-    const data = [["윤서연","SeoYeon"],["정혜린","HyeRin"],["이지우","JiWoo"],["김채연","ChaeYeon"],["김유연","YooYeon"],["김수민","SooMin"],["김나경","NaKyoung"],["공유빈","YuBin"],["카에데","Kaede"],["서다현","DaHyun"],["코토네","Kotone"],["곽연지","YeonJi"],["니엔","Nien"],["박소현","SoHyun"],["신위","Xinyu"],["마유","Mayu"],["린","Lynn"],["주빈","JooBin"],["정하연","HaYeon"],["박시온","ShiOn"],["김채원","ChaeWon"],["설린","Sullin"],["서아","SeoAh"],["지연","JiYeon"]]
+    const data = [["윤서연","SeoYeon","22aeff","fff"],["정혜린","HyeRin","9200ff","fff"],["이지우","JiWoo","fff800","000"],["김채연","ChaeYeon","98f21d","000"],["김유연","YooYeon","db0c74","fff"],["김수민","SooMin","fc83a4","000"],["김나경","NaKyoung","6799a0","fff"],["공유빈","YuBin","ffe3e2","000"],["카에데","Kaede","ffc935","000"],["서다현","DaHyun","ff9ad6","000"],["코토네","Kotone","ffde00","000"],["곽연지","YeonJi","5974ff","fff"],["니엔","Nien","ff953f","000"],["박소현","SoHyun","1222b5","fff"],["신위","Xinyu","d51313","fff"],["마유","Mayu","fe8e76","000"],["린","Lynn","ac62b7","fff"],["주빈","JooBin","b7f54c","000"],["정하연","HaYeon","52d9bb","000"],["박시온","ShiOn","ff428a","000"],["김채원","ChaeWon","c7a3e0","000"],["설린","Sullin","7bba8d","000"],["서아","SeoAh","cff3ff","000"],["지연","JiYeon","ffab62","000"]]
     const init=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
     const temp = init.map(v=>[v,Math.random()]).sort((x,y)=>x[1]-y[1]).map(v=>v[0])
     for (var i=0;i<8;i++){
@@ -38,7 +39,7 @@ function run() {
     }
     queue.push(['a5',0,0,0])
     
-    function display() {
+    function popping() {
         if (queue.length==0) {
             result()
             return 0
@@ -51,20 +52,25 @@ function run() {
         if (left && right) {
             queue.splice(queue.indexOf(popped),1)
             if (Math.random()<0.5) {
-                btn_left.innerHTML = `<p>${left[0]}</p><p>${left[1]}</p>`
-                btn_right.innerHTML = `<p>${right[0]}</p><p>${right[1]}</p>`
-                btn_left.addEventListener('click',select0)
-                btn_right.addEventListener('click',select1)
+                display(btn_left,left,select0)
+                display(btn_right,right,select1)
             } else {
-                btn_right.innerHTML = `<p>${left[0]}</p><p>${left[1]}</p>`
-                btn_left.innerHTML = `<p>${right[0]}</p><p>${right[1]}</p>`
-                btn_right.addEventListener('click',select0)
-                btn_left.addEventListener('click',select1)
+                display(btn_right,left,select0)
+                display(btn_left,right,select1)
             }
             return 0
         } else {
             return 1
         }
+    }
+
+    function display(element,source,handler) {
+        // element.innerHTML = `<p>${source[0]}</p><p>${source[1]}</p>`
+        element.innerText = source[language]
+        element.style.background = '#'+source[2]
+        element.style.color = '#'+source[3]
+        element.addEventListener('click',handler)
+        return 0
     }
     
     function callData(key) {
@@ -108,7 +114,7 @@ function run() {
         popped=[]
         span_progress.innerText = (progress*100/112).toFixed(1)+'%'
         span_progress.style.background = `linear-gradient(to right, #6e2cff ${progress*100/112}%, #000 ${progress*100/112}%)`
-        while (display()) {}
+        while (popping()) {}
         // select(0)
         return 0
     }
@@ -117,52 +123,65 @@ function run() {
         btn_left.remove()
         btn_right.remove()
         const final = obj.a6[0].map(v=>callData(v))
-        const content = final.map(v=>`<tr><td>${final.indexOf(v)+1}</td><td>${v.join(' ')}</td></tr>`).join('')
-        var table = `<table><thead><tr><th>순위</th><th>이름</th><tr></thead><tbody>${content}</tbody></table>`
+        const content = final.map(v=>`<tr><td>${final.indexOf(v)+1}</td><td style="background:#${v[2]};color:#${v[3]}">${v[language]}</td></tr>`).join('')
+        var table = `<table><thead><tr><th>${language?'Rank':'순위'}</th><th>${language?'Name':'이름'}</th><tr></thead><tbody>${content}</tbody></table>`
         span_progress.remove()
-        comment_progress.remove()
+        // comment_progress.remove()
         document.querySelector('footer').remove()
         var btn_save = document.createElement('button')
         var btn_save_wrap = document.createElement('div')
         btn_save.addEventListener('click',()=>{
             capture()
         })
-        btn_save.innerHTML = '<p>저장</p><p>Save</p>'
+        btn_save.innerHTML = language ? 'Save' : '저장'
         btn_save.style.cssText = 'margin:10px !important'
-        btn_save_wrap.style.width = '100%'
+        // btn_save_wrap.style.width = '100%'
         btn_save_wrap.append(btn_save)
         var tablewrap = document.createElement('table-wrap')
         tablewrap.innerHTML = table
         document.querySelector('article').append(btn_save_wrap)
         document.querySelector('article').append(tablewrap)
         document.querySelector('article').style.height = ''
+        return 0
     }
     
     async function capture() {
-        var span = document.createElement('p')
-        span.innerText = 'https://sort.wav.haus/'
-        document.querySelector('table-wrap').append(span)
+        var span1 = document.createElement('p')
+        span1.innerText = 'https://sort.wav.haus/'
+        span1.style.fontSize = '11px'
+        var span2 = document.createElement('p')
+        span2.innerText = 'tripleS Sort'
+        span2.style.fontWeight = 'bold'
+        span2.style.fontSize = '19px'
+        document.querySelector('table-wrap').append(span2)
+        document.querySelector('table-wrap').append(span1)
         var c = await html2canvas(document.querySelector('table-wrap'), {scale:2})
-        span.remove()
+        span1.remove()
+        span2.remove()
         c.toBlob(async blob=>{
             var downloadLink = document.createElement("a");
             downloadLink.download = 'tripleS_sort_'+new Date()*1;
             downloadLink.href = URL.createObjectURL(blob);
             downloadLink.click();
         })
+        return 0
     }
 
     function select0() {
         select(0)
+        return 0
     }
     function select1() {
         select(1)
+        return 0
     }
     
-    while (display()) {}
+    while (popping()) {}
 
-    document.querySelector('article').append(btn_left)
-    document.querySelector('article').append(btn_right)
+    document.querySelector('button-wrap').append(btn_left)
+    document.querySelector('button-wrap').append(btn_right)
     document.querySelector('footer').append(span_progress)
     document.querySelector('footer').append(comment_progress)
+
+    return 0
 }
